786d2ca0861c0faec5d6bd31d7e4164378def539
HuQingyang <hqy841440305@gmail.com>
2017-03-25 16:31:01 +0800

    Fix issues of webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,19 @@
-const path = require('path');
-
 module.exports = {
     entry: {
-        index: path.join(__dirname, './dist/index.js')
+        index: require('path').join(__dirname, './dist/index.js'),
     },
     output: {
-        path: path.join(__dirname, './src/'),
-        filename: '[name].build.js',
-        publicPath: "/assets/"
-    },
-    resolve: {
-        extensions: ['.js', '.jsx']
+        path: require('path').join(__dirname, './src/'),
+        filename: '[name].build.js'
     },
     module: {
         loaders: [{
-            test: '/\.js|jsx$/',
+            test: /\.js|jsx$/,
             loader: 'babel-loader',
             query: {
-                presets: ['es2015', 'react', 'stage-0']
+                presets: ['react', 'es2015'],
             }
         }]
     },
-    target: "web"
+    target: "electron",
 };
