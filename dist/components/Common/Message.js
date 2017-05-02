0c46f82b0defe99e61378d0c6bbb5135109f0ef1
HuQingyang <hqy841440305@gmail.com>
2017-05-02 17:41:58 +0800

    Supporting set config using gui.

---
+++ dist/components/Common/Message.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import reactCSS from 'reactcss';
+import eventProxy from '../../lib/eventProxy';
+
+
+export default class Message extends React.Component {
+    constructor(props) {
+        super(props);
+        this.style = this.style.bind(this);
+        this.showMessage = this.showMessage.bind(this);
+
+        this.state = {
+            messages: [],
+            message: false,
+        }
+    }
+
+    componentWillMount() {
+        eventProxy.on('message', function (message) {
+            this.state.messages.push(message);
+            this.showMessage();
+        }.bind(this))
+    }
+
+    showMessage() {
+        if (this.state.messages.length === 0 || this.state.message)
+            return;
+        this.setState((prevState) => ({
+            message: prevState.messages[0]
+        }));
+        setTimeout(function () {
+            this.setState((prevState) => ({
+                message: false
+            }));
+            this.state.messages.shift();
+            setTimeout(this.showMessage, 300);
+        }.bind(this), 1300)
+    }
+
+    render() {return (
+        <div style={this.style().container}>
+            <p style={this.style().message}>
+                {this.state.message}
+            </p>
+        </div>
+    )}
+
+    style() {return reactCSS({
+        default: {
+            container: {
+                width: 'calc(100% - 200px)',
+                marginLeft: '200px',
+                position: 'fixed',
+                display: 'flex',
+                bottom: '0',
+                flexDirection: 'row',
+                justifyContent: 'center',
+                justifyItems: 'center',
+                transition: 'all ease-out 300ms',
+                transform: `translateY(${
+                        this.state.message ? '-200px' : '100%'
+                    })`,
+                opacity: this.state.message ? '1' : '0'
+            },
+            message: {
+                bottom: '200px',
+                height: '35px',
+                width: 'fit-content',
+                maxWidth: '20%',
+                padding: '0 20px',
+                textAlign: 'center',
+                lineHeight: '35px',
+                borderRadius: '50px',
+                color: 'white',
+                backgroundColor: 'rgba(0, 0, 0, 0.7)',
+                fontSize: '1.2em',
+                letterSpacing: '0.1em',
+                cursor: 'pointer',
+                boxShadow: '0px 5px 50px 5px rgba(0,0,0,0.3)'
+            }
+        }
+    }, this.props, this.state)}
+}
---
+++ dist/components/Common/Select.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import reactCSS from 'reactcss';
+
+
+export default class Select extends React.Component {
+    constructor(props) {
+        super(props);
+        this.style = this.style.bind(this);
+        this.handleSelect = this.handleSelect.bind(this);
+        this.handleSpread = this.handleSpread.bind(this);
+
+        this.state = {
+            spread: false
+        }
+    }
+
+    handleSelect(value) {
+        this.props.onSelect(value);
+        this.handleSpread();
+    }
+
+    handleSpread() {
+        this.setState({spread: !this.state.spread})
+    }
+
+    render() {return (
+        <div style={this.style().container}>
+            <div style={this.style().default}>
+                {this.props.selects[this.props.default]}
+            </div>
+            {function () {
+                const selects = [];
+                let index = 0;
+                for (let name in this.props.selects)
+                    selects.push(
+                        <div
+                            onClick={this.handleSelect.bind(this, name)}
+                            style={Object.assign(this.style().select, {
+                                transform: this.state.spread ?
+                                    `translateY(${100 * ++index}%)` : 'none',
+                                color: this.props.default === name ?
+                                    '#42A5F0' : 'black'
+                            })}
+                            key={name}
+                        >{this.props.selects[name]}</div>
+                    )
+                return selects
+            }.bind(this)()}
+            <div
+                style={this.style().default}
+                onClick={this.setState.bind(this, {spread: !this.state.spread}, () => {})}
+            >
+                {this.props.selects[this.props.default]}
+            </div>
+        </div>
+    )}
+
+    style() {return reactCSS({
+        default: {
+            container: {
+                width: '100%',
+                height: this.state.spread ? `${40 * (this.props.selects.size() + 1)}px` : '40px',
+                transition: 'all ease 200ms',
+                position: 'relative',
+                boxShadow: '0px 3px 10px 1px rgba(0,0,0,0.21)',
+                overflow: 'hidden',
+                display: 'inline-block',
+                fontSize: '1em',
+                fontWeight: 'Bold',
+                letterSpacing: '0.05em',
+                zIndex: this.state.spread ? 11 : 3,
+            },
+            default: {
+                width: 'calc(100% - 10px)',
+                height: '40px',
+                lineHeight: '40px',
+                cursor: 'pointer',
+                textAlign: 'center',
+                backgroundColor: 'white',
+                position: 'absolute',
+                top: 0, left: 0,
+                borderLeft: '5px solid #42A5F0',
+            },
+            select: {
+                width: '100%',
+                height: '40px',
+                lineHeight: '40px',
+                cursor: 'pointer',
+                textAlign: 'center',
+                position: 'absolute',
+                top: 0, left: 0,
+                transition: 'all ease 300ms',
+                backgroundColor: 'white'
+            }
+        }
+    }, this.props, this.state)}
+}
