ceca73195950441bf74b34288f6bbfd812ef63a6
HuQingyang <hqy841440305@gmail.com>
2017-05-09 01:51:13 +0800

    Add windows installer generator.

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const electron = require('electron');
 const path = require('path');
 const url = require('url');
 const fs = require('node-fs-extra');
+const env = process.env.NODE_ENV;
 const platform = require('os').platform();
 const db = require('./src/js/db');
 const dataToHTML = require('./src/js/dataToHTML');
@@ -49,12 +50,14 @@ function openWindow(path, options, isMax) {
         win.show()
     });
 
-    const devToolPath = {
-        'win32': `C:\\Users\\HuQingyang\\AppData\\Local\\Google\\Chrome\\User Data\\Profile 1\\Extensions\\fmkadmapgofadopljbjfkapdkoienihi\\2.1.9_0`,
-        'darwin': `/Users/huqingyang/Library/Application Support/Google/Chrome/Default/Extensions/fmkadmapgofadopljbjfkapdkoienihi/2.1.9_0`,
-        'linux': '/home/hu/.config/google-chrome/Default/Extensions/fmkadmapgofadopljbjfkapdkoienihi/2.1.3_0'
-    }[platform];
-    BrowserWindow.addDevToolsExtension(devToolPath);
+    if (env === 'development') {
+        const devToolPath = {
+            'win32': `C:\\Users\\HuQingyang\\AppData\\Local\\Google\\Chrome\\User Data\\Profile 1\\Extensions\\fmkadmapgofadopljbjfkapdkoienihi\\2.1.9_0`,
+            'darwin': `/Users/huqingyang/Library/Application Support/Google/Chrome/Default/Extensions/fmkadmapgofadopljbjfkapdkoienihi/2.1.9_0`,
+            'linux': '/home/hu/.config/google-chrome/Default/Extensions/fmkadmapgofadopljbjfkapdkoienihi/2.1.3_0'
+        }[platform];
+        BrowserWindow.addDevToolsExtension(devToolPath);
+    }
     return win;
 }
 
