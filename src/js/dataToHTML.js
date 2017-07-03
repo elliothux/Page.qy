4c217cb688daa3b0b915b4f7d59b54f90607c2a2
胡青杨 <hqy841440305@gmail.com>
2017-07-03 13:37:53 +0800

    Fix an issue of template engine.

--- src/js/dataToHTML.js
+++ src/js/dataToHTML.js
@@ -309,7 +309,7 @@ function formatDate(date, language) {
         'Thursday', 'Friday', 'Saturday'];
     return {
         year: date.getFullYear(),
-        month: date.getMonth()+1 < 10 ? '0' + date.getMonth()+1 : date.getMonth(),
+        month: date.getMonth()+1 < 10 ? '0' + parseInt(date.getMonth()+1) : parseInt(date.getMonth()+1),
         date: date.getDate() < 10 ? '0' + date.getDate() : date.getDate(),
         hours: date.getHours() < 10 ? '0' + date.getHours() : date.getHours(),
         minutes: date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes(),
