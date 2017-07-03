a41663cdf42c2917f615d68b4e61da93c6d05f41
胡青杨 <hqy841440305@gmail.com>
2017-07-03 13:16:05 +0800

    Fix an issue of template engine.

--- src/js/templateEngine.js
+++ src/js/templateEngine.js
@@ -5,6 +5,7 @@ module.exports.parse = parse;
 
 function parse(rawData, template) {
     template = template.replace(/\<\!\-\-.*\-\-\>/g, '');
+    template = template.replace(/\<br\s*\/\>/g, '<br>');
     with (rawData) {
         let templateDOM = $.load(template);
         let match = templateDOM('template');
